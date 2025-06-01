80119bdc1158259a6dd36c88c4678a6f5bdea8b5
Eugene <skyzgamer@outlook.com>
2025-06-01 21:16:43 +0300

    web update, new resourcepack

--- src/components/Anarchy.jsx
+++ src/components/Anarchy.jsx
@@ -1,28 +1,32 @@
+// src/components/Anarchy.jsx
 import Detailed from "./Detailed";
 
-export default function anarchy() {
+export default function Anarchy() {
   const anarchyItems = [
     {
       title: "Мапа",
       description: "Перегляньте мапу анархії.",
-      image: "/news.png",
-      link: "/anarchy/faq",
+      image: "/map.png",
+      link: "#map",
     },
     {
-      title: "Додаткові правила анархії",
-      description: "Дізнайтесь більше про правила анархії.",
-      image: "/news.png",
-      link: "/anarchy/map",
+      title: "Правила анархії",
+      description: "Дізнайтесь більше про правила.",
+      image: "/images/anarchy/rules.png",
+      link: "#rules",
     },
     {
       title: "Донат",
-      description: "Не P2W Донат",
-      image: "/news.png",
+      description: "Не P2W: підтримай розвиток сервера.",
+      image: "/images/anarchy/donate.png",
       link: "/donate",
-    }
+    },
   ];
 
   return (
-    <Detailed items={anarchyItems} />
+    <div className="max-w-7xl mx-auto px-4 py-12 space-y-16">
+      {/* картки */}
+      <Detailed items={anarchyItems} />
+    </div>
   );
 }
