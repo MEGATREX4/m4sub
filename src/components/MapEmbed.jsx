6c15129b95361cc356b09da05c5b7635da1516ac
Yevhen <59817721+MEGATREX4@users.noreply.github.com>
2025-07-24 20:12:10 +0300

    Add fullscreen functionality to MapEmbed component

--- src/components/MapEmbed.jsx
+++ src/components/MapEmbed.jsx
@@ -1,13 +1,30 @@
 // src/components/MapEmbed.jsx
-export default function MapEmbed({ src }) {
+export default function MapEmbed({ src, fullscreenUrl }) {
     return (
-      <div className="w-full h-[80vh] md:h-[90vh]">
+      <div className="w-full h-[80vh] md:h-[90vh] relative">
         <iframe
           src={src}
           title="Embedded Map"
           className="w-full h-full border-0"
           allowFullScreen
         />
+        
+        {/* Optional fullscreen button positioned over the iframe */}
+        {fullscreenUrl && (
+          <div className="absolute bottom-4 right-4 z-10">
+            <a
+              href={fullscreenUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="minecraftFont bg-[#c5629a] hover:bg-[#b25587] text-white px-4 py-2 pixelated transition inline-block text-sm"
+              style={{
+                boxShadow: '2px 2px 0 #8b4570, 4px 4px 0 #000',
+              }}
+            >
+              🗺️ Відкрити на весь екран
+            </a>
+          </div>
+        )}
       </div>
     );
   }
--- src/components/SubserverMap.jsx
+++ src/components/SubserverMap.jsx
@@ -4,7 +4,10 @@ import MapEmbed from "./MapEmbed";
 export default function SubserverMap() {
   return (
     <div className="w-[calc(100vw-2rem)] max-w-7xl mx-auto px-4 py-12">
-      <MapEmbed src="http://map.m4sub.click/" />
+      <MapEmbed 
+        src="http://map.m4sub.click/" 
+        fullscreenUrl="http://sunrise.bubble.wtf:40010/"
+      />
     </div>
   );
 }
