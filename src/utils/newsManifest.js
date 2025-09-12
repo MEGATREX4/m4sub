bd31444b62a0eee71e124964b48193f0e1f64eb3
Yevhen <59817721+MEGATREX4@users.noreply.github.com>
2025-09-12 18:37:57 +0300

    Update sitemap.xml and add season-off_2025.md with new content

--- src/utils/newsManifest.js
+++ src/utils/newsManifest.js
@@ -5,6 +5,10 @@ export const newsManifest = [
     "pageLink": "independence_day_2025"
   },
   {
+    "filename": "season-off_2025.md",
+    "pageLink": "season-off_2025"
+  },
+  {
     "filename": "season5_archive_midseason.md",
     "pageLink": "season5_archive_midseason"
   },
